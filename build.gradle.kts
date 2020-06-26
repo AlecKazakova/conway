26aa49773eaf0b03a475702c2b89722df7d5ab4e
Alec Strong <astrong@squareup.com>
2020-06-26 17:45:58 -0400

    Initial Commit

---
+++ build.gradle.kts
@@ -0,0 +1,55 @@
+  
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+  base
+  kotlin("jvm") version "1.3.72"
+  id("org.jlleitschuh.gradle.ktlint") version "9.1.1"
+}
+
+repositories {
+  mavenCentral()
+}
+
+dependencies {
+  implementation(kotlin("stdlib-jdk8"))
+  implementation("com.github.ajalt", "clikt", "2.3.0")
+}
+
+tasks.withType<KotlinCompile> {
+  kotlinOptions.jvmTarget = "1.8"
+}
+
+val fatJar = task("fatJar", type = Jar::class) {
+  from(configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
+  with(tasks.jar.get() as CopySpec)
+
+  archiveClassifier.set("fat")
+
+  manifest {
+    attributes["Main-Class"] = "com.alecstrong.conway.Main"
+  }
+}
+
+val binaryJar = task("binaryJar") {
+  val binaryDir = File(buildDir, "bin")
+  val binaryFile = File(binaryDir, "conway")
+
+  doLast {
+    val fatJarFile = fatJar.archiveFile.get().asFile
+
+    binaryFile.parentFile.mkdirs()
+    binaryFile.appendText("#!/bin/sh\n\nexec java -jar \$0 \"\$@\"\n\n")
+    binaryFile.appendBytes(fatJarFile.readBytes())
+
+    binaryFile.setExecutable(true)
+  }
+}
+
+tasks {
+  "assemble" {
+    dependsOn(binaryJar)
+  }
+
+  binaryJar.dependsOn(fatJar)
+}
---
+++ src/main/kotlin/com/alecstrong/conway/main.kt
@@ -0,0 +1,107 @@
+@file:JvmName("Main")
+
+package com.alecstrong.conway
+
+import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.parameters.options.option
+import com.github.ajalt.clikt.parameters.options.split
+import java.io.InputStream
+import java.math.BigDecimal
+import java.math.RoundingMode
+
+fun main(vararg args: String) {
+  ConwayCli().main(args.toList())
+}
+
+internal class ConwayCli: CliktCommand(name = "conway") {
+  val folder by option("-D",
+      help = "Input directories <directory1>,<directory2>").split(",")
+  val since: String? by option("--since")
+
+  private val inFolderCommits = mutableMapOf<String, Int>()
+  private val allCommits = mutableMapOf<String, Int>()
+
+  private fun InputStream.forCommits(body: (email: String, changed: Int, file: String) -> Unit) {
+    reader().useLines { sequence ->
+      val iterator = sequence.iterator()
+      iterator.next() // Skip initial newline
+
+      while (iterator.hasNext()) {
+        val email = iterator.next()
+
+        iterator.next() // Skip newline
+
+        while (iterator.hasNext()) {
+          val linesChanged = iterator.next()
+          if (linesChanged.isEmpty()) break
+
+          try {
+            val (added, removed, file) = linesChanged.split(spaces)
+            body(email.substringBefore("@"),
+                ((added.toIntOrNull() ?: continue) + (removed.toIntOrNull() ?: continue)), file)
+          } catch (t: Throwable) {
+            continue
+          }
+        }
+      }
+    }
+  }
+
+  override fun run() {
+    val since = if (since == null) "" else "--since=\"$since\""
+
+    Runtime.getRuntime().exec(
+        arrayOf("git", "log", "--numstat", since, "--no-merges",
+            "--pretty=%n%ae")).inputStream.forCommits { email, changed, file ->
+      if (folder!!.any { file.startsWith(it) }) {
+        inFolderCommits[email] = inFolderCommits.getOrDefault(email, 0) + changed
+      }
+      allCommits[email] = allCommits.getOrDefault(email, 0) + changed
+    }
+
+    val totalInFolderCommits = inFolderCommits.map { it.value }.sum()
+
+    val contributions = allCommits
+        .mapNotNull { (author, commits) ->
+          if (commits < 10) return@mapNotNull null
+
+          val inFolder = inFolderCommits.getOrDefault(author, 0)
+          val percentageInFolders = inFolder
+              .toBigDecimal()
+              .multiply(BigDecimal.valueOf(100))
+              .divide(commits.toBigDecimal(), 2, RoundingMode.HALF_UP)
+
+          val percentageOfFolder = inFolder
+              .toBigDecimal()
+              .multiply(BigDecimal.valueOf(100))
+              .divide(totalInFolderCommits.toBigDecimal(), 2, RoundingMode.HALF_UP)
+
+          return@mapNotNull Contribution(author, percentageInFolders, percentageOfFolder)
+        }
+
+    contributions.sortedByDescending { it.percentageInFolder }
+        .forEach { (author, percentage, _) ->
+          if (percentage < BigDecimal.ONE) return@forEach
+          echo("$author writes ${percentage.toPlainString()}% of their code in $folder")
+        }
+
+    echo("---")
+
+    contributions.sortedByDescending { it.percentageOfFolder }
+        .forEach { (author, _, percentage) ->
+          if (percentage < BigDecimal.ONE) return@forEach
+          echo("$author accounts for ${percentage.toPlainString()}% of $folder")
+        }
+  }
+
+  private data class Contribution(
+    val author: String,
+    val percentageInFolder: BigDecimal,
+    val percentageOfFolder: BigDecimal
+  )
+
+  companion object {
+    private val spaces = Regex("\\s+")
+  }
+}
+
